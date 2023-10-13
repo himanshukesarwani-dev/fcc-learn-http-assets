141f0bff93d9d5ec613765c02863656699df9f67
HIMANSHUKESARWA\himanshukesarwani <himanshu.k@codecraft.co.in>
2023-10-13 10:24:04 +0530

    feat: solved 4-async/6-async

--- course/4-async/exercises/6-async/code.js
+++ course/4-async/exercises/6-async/code.js
@@ -1,27 +1,31 @@
-function getItemData() {
-  const response = await fetch('https://api.boot.dev/v1/courses_rest_api/learn-http/items', getSettings())
-  return response.json()
+async function getItemData() {
+  const response = await fetch(
+    "https://api.boot.dev/v1/courses_rest_api/learn-http/items",
+    getSettings()
+  );
+  const responseJSON = await response.json();
+  return responseJSON;
 }
 
 // don't touch below this line
 
-const items = await getItemData()
+const items = await getItemData();
 
-logItems(items)
+logItems(items);
 
 function getSettings() {
   return {
-    method: 'GET',
-    mode: 'cors',
+    method: "GET",
+    mode: "cors",
     headers: {
-      'X-API-Key': 'Testing',
-      'Content-Type': 'application/json'
-    }
-  }
+      "X-API-Key": "Testing",
+      "Content-Type": "application/json",
+    },
+  };
 }
 
 function logItems(items) {
   for (const item of items) {
-    console.log(item.name)
-  } 
+    console.log(item.name);
+  }
 }
